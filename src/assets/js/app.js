aab46f74205093a3e08c052fab383a36278ce1d2
andrei_labadaev <andrei-labadaev@mail.ru>
2021-12-31 12:37:42 +0300

    uppdate 12.40

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,10 +1,10 @@
 //= components/nouislider.min.js
 
 // open and close popup
-function popupOpen() {
+function popupOpen(openButtons) {
     let popupBg = document.querySelector('.popup__bg');
     let popup = document.querySelectorAll('.popup');
-    let openPopupButtons = document.querySelectorAll('.open-popup');
+    let openPopupButtons = document.querySelectorAll(openButtons);
     let closePopupButton = document.querySelectorAll('.close-popup');
 
     if (openPopupButtons) {
@@ -42,6 +42,9 @@ function popupOpen() {
         });
     }
 }
+
+
+
 
 // перелистование popup
 
@@ -84,4 +87,80 @@ function leafing() {
     }
 }
 leafing();
-popupOpen();
+popupOpen('.open-popup');
+popupOpen('.open-popup-cteation');
+
+// ползунок
+// rangeSliderId - id div
+// maxNum - максимальное занчение ползунка
+// minNum - минимальное значение ползунка
+//stepSlaid-шаг ползунка
+// inputNum - id input ввода-вывода значения ползнука
+//startNum - старровое положение ползунка
+rangeSlaider('input-range', 56, 0, 200, 1, 'input__rage-num-1');
+rangeSlaider('input-range-2', 3, 0, 6, 1, 'input__rage-num-2');
+function rangeSlaider(rangeSliderId, startNum, minNum, maxNum, stepSlaid, inputNum) {
+    const rangeSlider = document.getElementById(rangeSliderId);
+    if (rangeSlider) {
+        noUiSlider.create(rangeSlider, {
+            start: startNum,
+            connect: [true, false],
+            step: stepSlaid,
+            range: {
+                'min': minNum,
+                'max': maxNum
+            }
+        });
+
+        const inputNumber = document.getElementById(inputNum);
+        rangeSlider.noUiSlider.on('update', function (values) {
+            inputNumber.value = Math.round(values);
+        })
+
+        const setfilterSlaider = (value) => {
+            let arr = value;
+            rangeSlider.noUiSlider.set(arr);
+        }
+
+        inputNumber.addEventListener('change', (e) => {
+            setfilterSlaider(e.currentTarget.value);
+        })
+
+    }
+}
+
+// цвет  input color
+function inputColor() {
+    let inputColor = document.querySelectorAll('.creation__input-color');
+    if (inputColor) {
+        for (let i = 0; i < inputColor.length; i++) {
+            inputColor[i].parentElement.style.background = inputColor[i].value;
+        }
+        document.querySelector('body').addEventListener('click', () => {
+            let inputColor = document.querySelectorAll('.creation__input-color');
+            for (let i = 0; i < inputColor.length; i++) {
+                inputColor[i].parentElement.style.background = inputColor[i].value;
+            }
+        })
+    }
+}
+inputColor();
+
+// скрытый блок
+function inputhiden() {
+    let inputHiden = document.querySelector('.input__hiden');
+    let creationLabelCheckbox = document.querySelector('.creation__label-checkbox-h');
+    let creationLabel = document.querySelector('.creation__label');
+    if (creationLabelCheckbox) {
+        creationLabel.addEventListener('click', () => {
+            if (creationLabelCheckbox.checked) {
+                inputHiden.classList.remove('--hiden');
+            } else {
+                inputHiden.classList.add('--hiden');
+            }
+
+
+
+        })
+    }
+} inputhiden();
